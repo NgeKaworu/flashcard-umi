d0dd23785c8236bf2216fa4c49474113d753e9a9
fuRan <NgeKaworu@gmail.com>
2021-01-29 14:03:48 +0800

    console

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,10 @@
 export const qiankun = {
   // 应用加载之前
-  async bootstrap(props: any) {
-    console.log("bootstrap")
-  },
+  async bootstrap(props: any) {},
   // 应用 render 之前触发
-  async mount(props: any) {
-    console.log("mount")
-  },
+  async mount(props: any) {},
   // 应用卸载之后触发
-  async unmount(props: any) {
-    console.log("unmount")
-  },
+  async unmount(props: any) {},
   // loadMicroApp 方式加载微应用时生效
-  async update(props: any) {
-    console.log("update")
-  },
+  async update(props: any) {},
 };
